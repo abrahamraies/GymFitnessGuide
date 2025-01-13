3df2cec7c6d8e04596ea978950639bb6f03791cd
abrahamraies <abrahamraies@gmail.com>
2025-01-13 21:55:33 +1000

    Add ErrorHanlderMiddleware.

---
+++ GymFitnessGuide.API/ErrorHandlerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace GymFitnessGuide.API
+{
+    public class ErrorHandlerMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            HttpStatusCode status;
+            string errorMessage;
+
+            // Aca puedo agregar distintos tipos de excepciones.
+            if (exception is KeyNotFoundException)
+            {
+                status = HttpStatusCode.NotFound;
+                errorMessage = "The specified resource was not found.";
+            }
+            else if (exception is InvalidOperationException)
+            {
+                status = HttpStatusCode.BadRequest;
+                errorMessage = exception.Message;
+            }
+            else
+            {
+                status = HttpStatusCode.InternalServerError;
+                errorMessage = "An unexpected error occurred.";
+            }
+
+            response.StatusCode = (int)status;
+            var result = JsonConvert.SerializeObject(new { error = errorMessage });
+            return response.WriteAsync(result);
+        }
+    }
+}
